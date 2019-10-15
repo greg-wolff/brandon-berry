37add6b3cd393633f18a0a3ebe0fb396ee7f0151
greg-wolff <gregwwolff@gmail.com>
2019-10-14 18:48:20 -0700

    paused clouds vid

--- src/components/layout.js
+++ src/components/layout.js
@@ -92,7 +92,7 @@ const Header = props => {
           style={{ zIndex: 0, opacity: isHomepage ? 1 : 0 }}
         />
       }
-      <BackgroundVideo autoPlay muted loop>
+      <BackgroundVideo muted paused loop>
         <source src={props.video} type={`video/mp4`} />
       </BackgroundVideo>
       </HeaderContainer> 
